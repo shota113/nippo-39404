d541d1545965174bfa09b339fbdd6f96263172cd
shota113 <neverlose113@icloud.com>
2023-11-03 21:58:59 +0900

    userデーターベースの編集

--- db/migrate/20231031085828_devise_create_users.rb
+++ db/migrate/20231031085828_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :first_name_kana,    null: false
+      t.string :last_name_kana,     null: false
+      t.string :company,            null: false
+      t.string :number,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
