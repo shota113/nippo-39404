bf546ab84984b9290a4855561b37bc54ce5ecdea
shota113 <neverlose113@icloud.com>
2023-11-07 20:22:43 +0900

    モデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_31_085828) do
+ActiveRecord::Schema.define(version: 2023_11_06_140954) do
+
+  create_table "dairies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "place", null: false
+    t.text "work", null: false
+    t.timestamp "start_time", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.time "ending_time", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_dairies_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 2023_10_31_085828) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dairies", "users"
 end
