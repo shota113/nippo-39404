c4a4cce0f2dbcd31010e44e8388767ab826cc4b4
shota113 <neverlose113@icloud.com>
2023-11-07 20:04:00 +0900

    データベースの編集

--- db/migrate/20231106140954_create_dairies.rb
+++ db/migrate/20231106140954_create_dairies.rb
@@ -1,6 +1,12 @@
 class CreateDairies < ActiveRecord::Migration[6.0]
   def change
     create_table :dairies do |t|
+      t.references :user,            null: false, foreign_key: true
+      t.string :place,               null: false
+      t.text :work,                  null: false
+      t.timestamp :start_time        null: false
+      t.timestamp :ending_time       null: false
+      t.integer :category_id         null: false
 
       t.timestamps
     end
