db71134db11bedc194964c9e9b6afbc0feb3a42b
shota113 <neverlose113@icloud.com>
2023-11-17 19:59:17 +0900

    コントローラーの追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -19,15 +19,25 @@ class DiariesController < ApplicationController
     else
       render 'new'
     end
+  end
 
     def edit
+      @diary = Diary.find(params[:id])
       if @diary.user_id == current_user.id
       else
         redirect_to root_path
       end
     end
 
-  end
+    def update
+      @diary = Diary.find(params[:id])
+      if @diary.update(diary_params)
+        redirect_to diary_path(@diary)
+      else
+         render 'edit'
+      end
+    end
+
 
   def move_to_index
     unless user_signed_in?
