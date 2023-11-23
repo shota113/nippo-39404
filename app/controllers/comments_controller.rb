f0230cd21d9ef8b48e371985c4ef4089779bb8ff
shota113 <neverlose113@icloud.com>
2023-11-23 18:33:23 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to diary_path(params[:diary_id])
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, diary_id: params[:diary_id])
+  end
 end
